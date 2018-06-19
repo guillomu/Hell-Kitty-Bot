ef0b26925f2e49253c3f6fc3d76fc62c7aa378ea
GuiGui <groult_guigui@hotmail.com>
2018-06-20 01:48:59 +0200

    Ajout des fichier index.js img/ et des packages

---
+++ index.js
@@ -0,0 +1,37 @@
+const Discord = require('discord.js')
+
+const bot = new Discord.Client()
+
+// Au chargement du bot
+bot.on('ready', () => {
+  console.log(`Logged in as ${bot.user.tag}!`);
+
+  bot.user.setAvatar('./img/devilkitty.jpg') // On défini un avatar
+  .then(() => console.log('Avatar mis en place avec succès'))
+  .catch(console.error)
+
+  bot.user.setActivity('Hello Kitty Online', { type: 'PLAYING' }).catch(console.error) // On lui défini une activité
+});
+
+// A la réception d'un message
+bot.on('message', function (message) {
+    if(message.content === 'Chaton'){ // Si le message est égale à Chaton on envoie I <3 Cats!!!
+      message.reply('I <3 Cats!!!')
+    }
+})
+
+// Lorsqu'un nouveau membre entre dans la guild
+bot.on('guildMemberAdd', function(member) {
+  if(member.guild.channels.exists("name", "général")){ // Si le channel général existe
+    // On envoi un message sur le général pour lui souhaiter la bienvenue
+    member.guild.channels.find("name", "général").send(`Bienvenue ${member} dans le monde merveilleux de Hello Kitty Online.`)
+  }
+});
+
+// On créer un setInterval qui se relancera toute les 2 minutes
+setInterval(function(){
+  console.log('@everyone Trop Mignon!')
+  bot.channels.find("name", "général").send('@everyone Trop Mignon!') // On envoie @everyone Trop Mignon dans le channel général
+}, 120000);
+
+bot.login('NDU4NTg2NDQ4OTM4MDA4NTg2.Dgpzbg.MP-b4TUWOdVTmCTk7_HwkhzwIOY')  // Connection du bot sur le serveur
